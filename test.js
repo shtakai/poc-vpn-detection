cfcecb5157c419fb8f6b5367f7998e9678baf8bb
Nishanth Samala <nishanth.samala@gmail.com>
2020-02-05 01:05:24 -0500

    updated ui to show timezones

--- test.js
+++ test.js
@@ -6,14 +6,21 @@ function detectVPN() {
   .then(function (data) { 
     var ipTimezone = data.timezone
     console.log(`browser timezone: ${browserTimezone}`, `ip timezone: ${ipTimezone}`)
-    return ipTimezone != browserTimezone
+    return {
+      browser: browserTimezone,
+      ip: ipTimezone,
+      usingVPN: ipTimezone != browserTimezone
+    }
   })
 }
 
 detectVPN()
-.then(function(usingVPN) {
+.then(function(result) {
   var body = document.getElementById('body')
   var element = document.getElementById('detection-result')
-  var text = usingVPN ? element.textContent = "We know you're using a VPN" : element.textContent = "Ah, thanks for not using a VPN"
-  usingVPN ? body.style.backgroundColor = '#FF4343' : body.style.backgroundColor = '#00CC6A'
+  var text = result.usingVPN ? element.textContent = "We know you're using a VPN" : element.textContent = "Ah, thanks for not using a VPN"
+  result.usingVPN ? body.style.backgroundColor = '#FF4343' : body.style.backgroundColor = '#00CC6A'
+
+  document.getElementById("browser-timezone").textContent = result.browser
+  document.getElementById("ip-timezone").textContent = result.ip
 })
